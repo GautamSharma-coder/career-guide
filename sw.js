884e6c2ff1869efdddc6bfe71562497bd8748e36
GautamSharma-coder <gautamksharma45@gmail.com>
2025-09-10 23:10:57 +0530

    first commit

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "EduPath-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/favicon.png',
+  '/icons/favicon.png',
+  'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(keyList.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
+});
